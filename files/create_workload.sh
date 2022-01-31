f89771ec257a2e0ece54625d50d77437f243dc54
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-01 00:09:48 +0100

    adjusted the CLI arguments for the jar file

--- files/create_workload.sh
+++ files/create_workload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile 8hosts -queryFile single-groupby-5-8-1_8_hosts.csv
+java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile $1 -queryFile5m $2 -queryFile24h $3
