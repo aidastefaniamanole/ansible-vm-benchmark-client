9dc6dd372db6be4724a1847ff716cdf40efb80ac
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-02 13:44:42 +0100

    added scale parameter when running the run_data_generator script

--- files/create_workload.sh
+++ files/create_workload.sh
@@ -3,7 +3,7 @@
 SCALE=$1
 DAYS=$2
 
-(cd data-generation-tool/cmd/tsbs_generate_data/; ./run_data_generator.sh days $DAYS)
+(cd data-generation-tool/cmd/tsbs_generate_data/; SCALE=$SCALE ./run_data_generator.sh days $DAYS)
 (cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=576 ./run_query_generator.sh)
 (cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=2 ./run_query_generator.sh)
 java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile ${SCALE}hosts \
