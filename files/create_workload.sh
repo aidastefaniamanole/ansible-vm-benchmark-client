2fb25a59c44de5aaf2bb088e47a4b78dcc2f6c5b
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-02 11:54:27 +0100

    updated the create_workload script

--- files/create_workload.sh
+++ files/create_workload.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile $1 -queryFile5m $2 -queryFile24h $3
+SCALE=$1
+DAYS=$2
+
+(cd data-generation-tool/cmd/tsbs_generate_data/; ./run_data_generator.sh days $DAYS)
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=576 ./run_query_generator.sh)
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=2 ./run_query_generator.sh)
+java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile ${SCALE}hosts \
+    -queryFile5m single-groupby-all-all-5m.csv \
+    -queryFile24h double-groupby-all.csv
