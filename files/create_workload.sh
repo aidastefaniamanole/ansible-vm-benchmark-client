94deaf5c9ef1d90351339482fa46e72363a0e977
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-04 23:54:54 +0100

    changed experiment parameters to the 14 days config

--- files/create_workload.sh
+++ files/create_workload.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo -e "Specify:\n - arg1 -> scale of the experiment\n - arg2 -> the number of simulated days"
+    exit 1
+fi
+
 SCALE=$1
 DAYS=$2
 
 (cd data-generation-tool/cmd/tsbs_generate_data/; SCALE=$SCALE ./run_data_generator.sh days $DAYS)
-(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=576 ./run_query_generator.sh)
-(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=2 ./run_query_generator.sh)
-java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile ${SCALE}hosts \
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=4032 ./run_query_generator.sh)
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=14 ./run_query_generator.sh)
+java -Xmx8g -jar benchmark-client-tool -queryMixer -insertFile ${SCALE}hosts \
     -queryFile5m single-groupby-all-all-5m.csv \
     -queryFile24h double-groupby-all.csv
--- files/run_experiment.sh
+++ files/run_experiment.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -Xmx2g -jar benchmark-client-tool -preloadPhaseDuration 10 -noWorkers 8 -workloadFile workload.txt -vmClusterIpAddr inventory_benchmark_client
+if [ $# -ne 1 ]; then
+    echo "Specify the number of clients to run the benchmark with"
+    exit 1
+fi
+
+java -Xmx8g -jar benchmark-client-tool -noWorkers $1 -workloadFile workload.txt -vmClusterIpAddr inventory_benchmark_client
