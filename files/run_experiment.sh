326437e1d9c65c0361ca0d5c20baff42d211e3b9
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-04 23:56:28 +0100

    added no of parameters check to the experiment scripts

--- files/run_experiment.sh
+++ files/run_experiment.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -Xmx2g -jar benchmark-client-tool -noWorkers 100 -workloadFile workload.txt -vmIpAddr $1
+if [ $# -ne 1 ]; then
+    echo "Specify the number of clients to run the benchmark with"
+    exit 1
+fi
+
+java -Xmx8g -jar benchmark-client-tool -noWorkers $1 -workloadFile workload.txt -vmIpAddr $1
