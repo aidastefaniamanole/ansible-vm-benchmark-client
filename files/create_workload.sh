57fb910eda4d99e5fc084c72cdc1c5336412e090
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-02-04 23:44:29 +0100

    changed experiment parameters in the scripts to the 14 days configuration

--- files/create_workload.sh
+++ files/create_workload.sh
@@ -4,8 +4,8 @@ SCALE=$1
 DAYS=$2
 
 (cd data-generation-tool/cmd/tsbs_generate_data/; SCALE=$SCALE ./run_data_generator.sh days $DAYS)
-(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=576 ./run_query_generator.sh)
-(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=2 ./run_query_generator.sh)
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=single-groupby-all-all-5m SCALE=$SCALE QUERIES=4032 ./run_query_generator.sh)
+(cd data-generation-tool/cmd/tsbs_generate_queries/; QUERY=double-groupby-all SCALE=$SCALE QUERIES=14 ./run_query_generator.sh)
 java -Xmx2g -jar benchmark-client-tool -queryMixer -insertFile ${SCALE}hosts \
     -queryFile5m single-groupby-all-all-5m.csv \
     -queryFile24h double-groupby-all.csv
